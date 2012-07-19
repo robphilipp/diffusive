85ce457bc96fb17b0e25913f724c5bd66c8531cd
robphilipp <devnull@localhost>
2012-07-18 20:55:30 -0400

    documented the configuration class and cleaned up a bit

--- src/org/microtitan/diffusive/launcher/config/RestfulDiffuserConfig.java
+++ src/org/microtitan/diffusive/launcher/config/RestfulDiffuserConfig.java
@@ -1,6 +1,5 @@
 package org.microtitan.diffusive.launcher.config;
 
-import java.lang.reflect.Method;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,22 +14,46 @@ import org.microtitan.diffusive.diffuser.restful.resources.RestfulClassPathResou
 import org.microtitan.diffusive.diffuser.restful.resources.RestfulDiffuserManagerResource;
 import org.microtitan.diffusive.diffuser.serializer.Serializer;
 import org.microtitan.diffusive.diffuser.serializer.SerializerFactory;
+import org.microtitan.diffusive.launcher.DiffusiveLauncher;
 
+/**
+ * Configuration class used to configure the Restful Diffuser framework. The method annotated
+ * with {@link DiffusiveConfiguration} is the method that will be called by the {@link DiffusiveLauncher} 
+ * to configure the framework. Any static method can be used in this manner. For example, one could
+ * write a static method that reads from a configuration file and then performs the necessary configuration.
+ * 
+ * @author Robert Philipp
+ * Jul 18, 2012
+ */
 public class RestfulDiffuserConfig {
 	
-	// holds the list of client endpoints...for testing we use the same address as specified
-	// in the RestfulDiffuserServer as the default server URI.
+	// holds the list of client endpoints to which diffused methods are sent. the host must
+	// have a restful diffusive server running that can accept requests.
 	public static final List< String > CLIENT_ENDPOINTS = Arrays.asList( RestfulDiffuserServer.DEFAULT_SERVER_URI );
 //	public static final List< String > CLIENT_ENDPOINTS = Arrays.asList( "http://192.168.1.8:8182" );
 	
 	// holds the base URI of the class path that gets passed to the remote diffuser manager when
-	// creating a diffuser. this allows the remote code to load classes from a remote server.
+	// creating a diffuser. this allows the remote code to load classes from a remote server. This
+	// URI typically points to the host running that is launching the code to be diffused (since it
+	// is that code that has the classes), or some other host that holds all the required classes (that
+	// have been deployed there) and is running a restful diffuser server
 	public static final List< String > CLASSPATH_URI = Arrays.asList( "http://192.168.1.4:8182" );
 	
+	/**
+	 * Method that is called to configure the Diffusive framework. In particular, creates a 
+	 * {@link RestfulDiffuser} with the specified diffusion endpoints and the class path URI
+	 * passed to the remote restful diffuser so that it can load classes for which it needs to 
+	 * run diffused methods.
+	 * 
+	 * Sets the default diffuser into the {@link KeyedDiffuserRepository}. This is a required step
+	 * because the byte code engineering needs to know which diffuser to use, and the repository is
+	 * the location holding that diffuser.
+	 */
 	@DiffusiveConfiguration
 	public static final void configure()
 	{
-		// load the diffuser repository
+		// create a default diffuser, load the diffuser repository, and set the defualt diffuser
+		// into the repository (needed by the Javassist diffuser method replacement)
 		final Serializer serializer = SerializerFactory.getInstance().createSerializer( SerializerFactory.SerializerType.OBJECT.getName() );
 		final List< URI > clientEndpoints = createEndpointList();
 		final List< URI > classPaths = createClassPathList();
@@ -38,7 +61,11 @@ public class RestfulDiffuserConfig {
 		KeyedDiffuserRepository.getInstance().setDiffuser( diffuser );
 	}
 	
-	public static final List< URI > createEndpointList()
+	/*
+	 * @return a {@link List} of {@link URI} that hold the location of endpoints to which the
+	 * local {@link RestfulDiffuser} can diffuse method calls.
+	 */
+	private static List< URI > createEndpointList()
 	{
 		final List< URI > endpoints = new ArrayList<>();
 		for( String client : CLIENT_ENDPOINTS )
@@ -48,7 +75,11 @@ public class RestfulDiffuserConfig {
 		return endpoints;
 	}
 	
-	public static final List< URI > createClassPathList()
+	/*
+	 * @return a {@link List} of {@link URI} holding the location from which a {@link RestfulClassLoader}
+	 * can load local classes. To each URI in the list, the resource path is appended.
+	 */
+	private static List< URI > createClassPathList()
 	{
 		final List< URI > endpoints = new ArrayList<>();
 		for( String client : CLASSPATH_URI )
@@ -56,20 +87,5 @@ public class RestfulDiffuserConfig {
 			endpoints.add( URI.create( client + RestfulClassPathResource.CLASSPATH_PATH ) );
 		}
 		return endpoints;
-	}
-	
-	public static void main( String[] args ) throws Exception
-	{
-		for( Method m : RestfulDiffuserConfig.class.getMethods() )
-		{
-			if( m.isAnnotationPresent( DiffusiveConfiguration.class ) )
-			{
-				System.out.println( "Found annotation on method: " + m.getName() );
-			}
-			else
-			{
-				System.out.println( "Method not annotaed with @" + DiffusiveConfiguration.class.getName() + ": " + m.getName() );
-			}
-		}
 	}
 }
