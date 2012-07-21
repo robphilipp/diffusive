d02a77f4d049f6a5e17234e4991a26c6ea7390fa
robphilipp <devnull@localhost>
2012-07-21 19:14:22 -0400

    cleaned unused imports.

--- src/org/microtitan/diffusive/diffuser/serializer/ObjectSerializer.java
+++ src/org/microtitan/diffusive/diffuser/serializer/ObjectSerializer.java
@@ -4,9 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.ObjectStreamClass;
 import java.io.OutputStream;
 
 import org.apache.commons.io.input.ClassLoaderObjectInputStream;
