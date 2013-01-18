16e41f5ecb47572ada2984ae79c7fdb184b6ff2f
robphilipp001 <devnull@localhost>
2013-01-18 17:56:30 -0500

    Continued work on website.

--- docs/diffusive_web/WebContent/scripts/setup.js
+++ docs/diffusive_web/WebContent/scripts/setup.js
@@ -1,48 +1,37 @@
 function setup( tocDepth ) {
-    $(document).ready( function() {
+    // create the table of contents
+    tocDepth = tocDepth || 3;
+    createToc( "sidebar-nav", tocDepth, "Contents" );
 
-        // create the table of contents
-        tocDepth = tocDepth || 3;
-        createToc( "sidebar-nav", tocDepth, "Contents" );
+    $(function() {
+        $( "#tabs" ).tabs();
+    });
 
-        $(function() {
-            $( "#tabs" ).tabs();
-            $( "#accordion" ).accordion( {
-                autoHeight: false,
-                header: "h3",
-                collapsible: true,
-                active: false
-            } );
-            $( "#whats-new-accordion" ).accordion( { autoHeight: false } );
-        });
+    // syntax highlighting for the code
+    SyntaxHighlighter.all( {
+        "gutter": false,
+        "toolbar": false
+    } );
 
-        // syntax highlighting for the code
-        SyntaxHighlighter.all( {
-            "gutter": false,
-            "toolbar": false
-        } );
+    $( "a.scroll-offset" ).click( function() {
+        var element = $( this ).attr( "href" );
+        if( element ) {
+            scrollToDiv( $( element ), 81 );
+        }
+        return false;
+    });
 
-        $( "a.scroll-offset" ).click( function() {
-            var element = $( this ).attr( "href" );
-            if( element ) {
-                scrollToDiv( $( element ), 81 );
-            }
-            return false;
-        });
-
-        $( ".sidebar-nav" ).affix( {
-            offset: {
-                top: function() {
-                    var width = $( window ).width();
-                    if( width > 768 && width < 979 ) {
-                        return $( ".navbar-fixed-top" ).height();
-                    } else {
-                        return 0;
-                    }
-                },
-                bottom: 0
-            }
-        } );
-
-    }, false );
+    $( ".sidebar-nav" ).affix( {
+        offset: {
+            top: function() {
+                var width = $( window ).width();
+                if( width > 768 && width < 979 ) {
+                    return $( ".navbar-fixed-top" ).height();
+                } else {
+                    return 0;
+                }
+            },
+            bottom: 0
+        }
+    } );
 }
