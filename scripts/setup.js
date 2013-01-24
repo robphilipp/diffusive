ea86dcaabd6c1c041040d409720200f6d07fd572
Rob Philipp <rob.philipp@gmail.com>
2013-01-24 10:44:56 -0500

    testing link to download file

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,7 +1,9 @@
 function setup( tocDepth ) {
     // create the table of contents
     tocDepth = tocDepth || 3;
-    createToc( "sidebar-nav", tocDepth, "Contents" );
+    if( tocDepth > 0 ) {
+        createToc( "sidebar-nav", tocDepth, "Contents" );
+    }
 
     $(function() {
         $( "#tabs" ).tabs();
