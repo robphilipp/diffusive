2068b14049c0a7b6f576ede9b19ff5600ff74a30
Rob Philipp <rob.philipp@gmail.com>
2013-02-01 18:59:43 -0500

    Continued to refactor the create-diffuser-form for the web app. Now can have multiple forms, which I'll use in the diffuser list for editing the properties of a diffuser. Basic checks pan out.

--- manager/WebContent/scripts/diffuser-form.js
+++ manager/WebContent/scripts/diffuser-form.js
@@ -1,32 +1,51 @@
 /**
- * User: rob
+ * User: Robert Philipp
  * Date: 1/7/13
  * Time: 9:08 AM
  */
-function DiffuserForm( parentId, diffusersUri, settings ) {
+/**
+ * Creates a diffuser form object for creating diffusers. This object manages all aspects
+ * of communicating with the RESTful diffuser server to create the diffuser.
+ * @param parent The jQuery element into which the form should be placed
+ * @param formId The ID of the form element to be created. If you need more than one of these
+ * forms in one page, then you should assign a unique name to the form. All the elements in
+ * this form use this ID to ensure that its elements have unique IDs
+ * @param serverUri The base URI to the RESTful diffuser server
+ * @param settings Overrides to the default settings found in the config field
+ * @constructor
+ */
+function DiffuserForm( parent, serverUri, formId, settings ) {
+
+    if( !formId ) {
+        formId = "create-diffuser-form-";
+    } else {
+        formId += "-";
+    }
 
     var config = {
-        formId : "create-diffuser-form",
-        containingClassId : "containing-class-input",
-        methodNameId : "method-name-input",
-        returnTypeId : "return-type-list",
-        methodArgListId : "method-argument-list",
-        classPathListId : "class-path-list",
-        endPointListId : "end-points-list",
-        serializerId : "serializer-name-list",
+        containingClassId : formId + "containing-class-input",
+        methodNameId : formId + "method-name-input",
+        returnTypeId : formId + "return-type-list",
+        methodArgListId : formId + "method-argument-list",
+        classPathListId : formId + "class-path-list",
+        endPointListId : formId + "end-points-list",
+        serializerId : formId + "serializer-name-list",
         defaultSerializerName : "persistence_xml",
 
-        addArgTypeButtonId : "add-arg-type-button",
+        addArgTypeButtonId : formId + "add-arg-type-button",
 
-        addReturnTypeButtonId : "add-return-type-button",
-        removeReturnTypeButtonId : "remove-return-type-button",
+        addReturnTypeButtonId : formId + "add-return-type-button",
+        removeReturnTypeButtonId : formId + "remove-return-type-button",
 
-        addClassPathButtonId : "add-class-path-button",
+        addClassPathButtonId : formId + "add-class-path-button",
 
-        addEndpointButtonId : "add-end-points-button",
+        addEndpointButtonId : formId + "add-end-points-button",
 
-        submitButtonId: "create-diffuser-submit-button",
-        resetButtonId : "create-diffuser-reset-button",
+        submitButtonId: formId + "create-diffuser-submit-button",
+        resetButtonId : formId + "create-diffuser-reset-button",
+
+        diffuserPath: "/diffusers",
+        serializerPath: "/serializers",
 
         successCallback: function() {}
     };
@@ -35,39 +54,43 @@ function DiffuserForm( parentId, diffusersUri, settings ) {
 
     // class identifiers used for list items, the input text fields contained in the list items, and the buttons for
     // removing a list item. these are used for method argument, return type, class paths, and end points
-    var listItemClass = "list-item";
-    var listItemInputClass = "item-list-input";
-    var listItemRemoveButtonClass = "item-list-remove-button";
+    var listItemClass = formId + "list-item";
+    var listItemInputClass = formId + "item-list-input";
+    var listItemRemoveButtonClass = formId + "item-list-remove-button";
 
-    var className = "class-name";
-    var methodName = "method-name";
-    var variableName = "variable-name";
-    var serializerName = "serializer-name";
+    var className = formId + "class-name";
+    var methodName = formId + "method-name";
+    var variableName = formId + "variable-name";
+    var serializerNameId = formId + "serializer-name";
 
-    var returnType = "return-type";
-    var classPaths = "class-paths";
-    var endPoints = "end-points";
+    var returnTypeId = formId + "return-type";
+    var classPathsId = formId + "class-paths";
+    var endPointsId = formId + "end-points";
 
     var formElementName = "form-element";
 
     // merge the configuration items specified in the function and the default ones
     $.extend( config, settings );
 
+    var diffuserUri = serverUri + config.diffuserPath;
+    var serializerUri = diffuserUri + config.serializerPath;
+
     //
     // create the diffuser form
     //
-    var $form = $( "<form></form>", { "id": config.formId } );
+    parent.append( createDiffuserForm( formId ) );
 
-    createDiffuserForm();
+    // add the list of the serializer names from the server and place them in the select UI element
+    getSerializerList( serializerUri, $( "#" + config.serializerId ), config.defaultSerializerName );
 
     // make the item-lists (arguments, end-points, class-paths) sortable through drag and drop
     $( ".sortable" ).sortable();
 
     // set up the validator
-    $( "#" + config.formId ).validate({
+    $( "#" + formId ).validate({
         submitHandler: function() {
             $.ajax({
-                url: diffusersUri,
+                url: diffuserUri,
                 type: "PUT",
                 data: asJson(),
                 dataType: "xml",
@@ -111,17 +134,23 @@ function DiffuserForm( parentId, diffusersUri, settings ) {
 
     // add argument input field and remove-item button to the method's argument list
     $( "#" + config.addArgTypeButtonId ).click( function() {
-        addListItem( config.methodArgListId, variableName, "parameter type (e.g. double, java.lang.String)" )
+       $( "#" + config.methodArgListId ).append( createListItem( variableName, "parameter type (e.g. double, java.lang.String)" ) );
     });
-//    $( "#" + config.addArgTypeButtonId ).hover( function() {
-//        $( "i", $( this ) ).toggleClass( 'icon-plus-sign' );
-//    });
+    $( "#" + config.addArgTypeButtonId ).hover( function() {
+        $( "i", $( this ) ).toggleClass( 'icon-plus-sign' );
+    });
 
     // add class-path input field and remove-item button to the class-path list
-    $( "#" + config.addClassPathButtonId ).click( function() { addClasspathItem( config.classPathListId ) } );
+//    $( "#" + config.addClassPathButtonId ).click( function() { addClasspathItem( config.classPathListId ) } );
+    $( "#" + config.addClassPathButtonId ).click( function() {
+        $( "#" + config.classPathListId ).append( createClasspathItem() );
+    });
 
     // add end-point input field and remove-item button to the end-point list
-    $( "#" + config.addEndpointButtonId ).click( function() { addEndpointItem( config.endPointListId ) } );
+//    $( "#" + config.addEndpointButtonId ).click( function() { addEndpointItem( config.endPointListId ) } );
+    $( "#" + config.addEndpointButtonId ).click( function() {
+        $( "#" +  config.endPointListId ).append( createEndpointItem() );
+    });
 
     // remove the item from the list when the associated button is pressed
     $( "." + listItemRemoveButtonClass ).live( "click", function() {
@@ -141,7 +170,7 @@ function DiffuserForm( parentId, diffusersUri, settings ) {
                     '<button id="' + config.removeReturnTypeButtonId + '" class="btn"><i class="icon-remove"></i></button>' +
                 '</div>' +
                 '</li>' );
-            $( "i", this ).toggleClass( 'icon-white' );
+            $( "i", this ).attr( 'class', 'icon-plus icon-white' );
             $( "#" + config.removeReturnTypeButtonId ).hover( function() {
                 $( "i", this ).toggleClass( 'icon-remove-sign' );
             });
@@ -151,7 +180,7 @@ function DiffuserForm( parentId, diffusersUri, settings ) {
 
     // allows the user to remove the return type; once clicked enables the add-return-type button
     $( "#" + config.removeReturnTypeButtonId ).live( "click", function() {
-        $( "i", "#" + config.addReturnTypeButtonId ).toggleClass( 'icon-white' );
+        $( "i", "#" + config.addReturnTypeButtonId ).attr( 'class', 'icon-plus' );
         $( this ).parent().parent().remove();
         $( "#" + config.addReturnTypeButtonId ).removeAttr( "disabled" );
     });
@@ -163,7 +192,10 @@ function DiffuserForm( parentId, diffusersUri, settings ) {
     /**
      * Creates the diffuser form HTML code and adds the div specified as the method argument to the containing function
      */
-    function createDiffuserForm() {
+    function createDiffuserForm( formId ) {
+
+        var $form = $( "<form></form>", { "id": formId } );
+
         // create the fist div containing the signature information
         var $div = $( "<div></div>" ).addClass( formElementName );
 
@@ -178,7 +210,11 @@ function DiffuserForm( parentId, diffusersUri, settings ) {
 
         // method name
         $para = $( "<p>Method Name: </p>", { id: config.methodNameId } );
-        $( "<input>", { id: config.methodNameId, type: "text", class: "span5" } ).addClass( "required " + methodName ).appendTo( $para );
+        $( "<input>", { id: config.methodNameId,
+                        type: "text",
+                        class: "span5",
+                        placeholder: "The name of the method to diffuse"
+                      } ).addClass( "required " + methodName ).appendTo( $para );
         $para.appendTo( $div );
 
         // method arguments
@@ -189,7 +225,7 @@ function DiffuserForm( parentId, diffusersUri, settings ) {
         $para.appendTo( $div );
 
         // return type
-        $para = $( "<p></p>", { id: returnType } );
+        $para = $( "<p></p>", { id: returnTypeId } );
         $( '<a id="' + config.addReturnTypeButtonId + '" href="#"><i class="icon-plus"></i></a>' ).appendTo( $para );
         $para.append( "Return Type" );
         $( "<ul></ul>", { id: config.returnTypeId } ).addClass( "sortable" ).appendTo( $para );
@@ -199,7 +235,7 @@ function DiffuserForm( parentId, diffusersUri, settings ) {
 
         // div holding the class paths
         $div = $( "<div></div>" ).addClass( formElementName );
-        $para = $( "<p></p>", { id: classPaths } );
+        $para = $( "<p></p>", { id: classPathsId } );
         $( '<a id="' + config.addClassPathButtonId + '" href="#"><i class="icon-plus"></i></a>' ).appendTo( $para );
         $para.append( "Class-Path" );
         $( "<ul></ul>", { id: config.classPathListId } ).addClass( "sortable" ).appendTo( $para );
@@ -208,7 +244,7 @@ function DiffuserForm( parentId, diffusersUri, settings ) {
 
         // div holding the additional endpoints
         $div = $( "<div></div>" ).addClass( formElementName );
-        $para = $( "<p></p>", { id: endPoints } );
+        $para = $( "<p></p>", { id: endPointsId } );
         $( '<a id="' + config.addEndpointButtonId + '" href="#"><i class="icon-plus"></i></a>' ).appendTo( $para );
         $para.append( "Additional Diffuser End-Points" );
         $( "<ul></ul>", { id: config.endPointListId } ).addClass( "sortable" ).appendTo( $para );
@@ -217,8 +253,9 @@ function DiffuserForm( parentId, diffusersUri, settings ) {
 
         // div containing the serializer name and option selection
         $div = $( "<div></div>" ).addClass( formElementName );
-        $( "<p>Serializer Name: </p>", { id: serializerName } ).appendTo( $div );
-        $( "<select></select>", { id: config.serializerId } ).appendTo( $div );
+        $( "<p>Serializer Name: </p>", { id: serializerNameId } ).appendTo( $div );
+        var serializerListElem = $( "<select></select>", { id: config.serializerId } )
+        $div.append( serializerListElem );
         $div.appendTo( $form );
 
         // div containing the buttons
@@ -227,13 +264,18 @@ function DiffuserForm( parentId, diffusersUri, settings ) {
         $( "<input>", { type: "button", id: config.resetButtonId, value: "Reset" } ).appendTo( $div );
         $div.appendTo( $form );
 
-        $form.appendTo( "#" + parentId );
+        return $form;
     }
 
     /**
      * resets the create-diffuser form and add the handler
      */
     function resetDiffuserForm() {
+
+        // removes the return-type text field and adds the add-return-type button back, in case it was gone
+        $( "#" + config.removeReturnTypeButtonId ).parent().parent().remove();
+        $( "#" + config.addReturnTypeButtonId ).removeAttr( "disabled" );
+        $( "i", "#" + config.addReturnTypeButtonId ).attr( 'class', 'icon-plus' );
 
         // sets all the form's text input fields to empty
         $( "#" + config.containingClassId ).val( "" );
@@ -242,19 +284,16 @@ function DiffuserForm( parentId, diffusersUri, settings ) {
         // removes all the form's input fields from the lists
         $( "." + listItemClass ).remove();
         $( "." + listItemInputClass ).parent().remove();
-
-        // adds the add-return-type button back, in case it was gone
-        $( "#" + config.addReturnTypeButtonId ).removeAttr( "disabled" );
     }
 
     /**
      * Returns the values from the input elements with class specified in the listItemInputClass
-     * for the list element with the specified listId
-     * @param listId The ID of the list holding the input elements
+     * for the list element with the specified list
+     * @param list The jQuery element representing the list holding the input elements
      * @return {Array} The values of the input elements
      */
-    function getValues( listId ) {
-        var argElems = $( "#" + listId ).find( "." + listItemInputClass ).toArray();
+    function getValues( list ) {
+        var argElems = list.find( "." + listItemInputClass ).toArray();
         var arguments = [];
         for( var i = 0; i < argElems.length; ++i ) {
             arguments.push( $(argElems[ i ]).val() );
@@ -269,7 +308,7 @@ function DiffuserForm( parentId, diffusersUri, settings ) {
         var argElems = $( "#" + config.returnTypeId ).find( "." + listItemInputClass ).toArray();
         var argument = "void";
         if( argElems.length > 0 ) {
-            argument = $(argElems[ 0 ]).val()
+            argument = $( argElems[ 0 ] ).val()
         }
         return argument;
     }
@@ -284,36 +323,42 @@ function DiffuserForm( parentId, diffusersUri, settings ) {
     /**
      * Adds an end-point item to the list of end-points
      * @param selector The class selector into which the new list item will be appended
+     * @return {*|jQuery|HTMLElement}
      */
-    function addEndpointItem( selector ) {
-        addListItem( selector, "url", "http://remote.ip.address:8182/diffuser" );
+    function createEndpointItem() {
+        return createListItem( "url", "http://remote.ip.address:8182/diffuser" );
     }
 
     /**
      * Adds an end-point item to the list of end-points
      * @param selector The class selector into which the new list item will be appended
+     * @return {*|jQuery|HTMLElement}
      */
-    function addClasspathItem( selector ) {
-        addListItem( selector, "url", "http://remote.ip.address:8182/classpath" );
+    function createClasspathItem() {
+        return createListItem( "url", "http://remote.ip.address:8182/classpath" );
     }
+
     /**
-     * Adds a list item with a sortable handle and a remove button. Intended for the method argument type list,
-     * the class path list, and the end-point list
+     * Creates a jQuery element representing the list item with a sortable handle and a remove button.
+     * Intended for the method argument type list, the class path list, and the end-point list.
      * @param selector The class selector into which the new list item will be appended
      * @param validationClass The validation type for the text field
      * @param placeholder The place holder value in the text field
+     * @return {*|jQuery|HTMLElement}
      */
-    function addListItem( selector, validationClass, placeholder ) {
+    function createListItem( validationClass, placeholder ) {
         if( !placeholder ) {
             placeholder = " ";
         }
-        $( "#" + selector ).append( '<li class="' + listItemClass + '">' +
-            '<span class="ui-icon ui-icon-arrowthick-2-n-s"></span>' +
-            '<div class="input-append">' +
-                '<input type="text" class="' + listItemInputClass + ' ' + validationClass + ' span8" placeholder="' + placeholder + '">' +
-                '<button class="btn '+ listItemRemoveButtonClass + '" type="button"><i class="icon-remove"></i></button>' +
-            '</div>' +
-            '</li>' );
+        var item =
+            '<li class="' + listItemClass + '">' +
+                '<span class="ui-icon ui-icon-arrowthick-2-n-s"></span>' +
+                '<div class="input-append">' +
+                    '<input type="text" class="' + listItemInputClass + ' ' + validationClass + ' span8" placeholder="' + placeholder + '">' +
+                    '<button class="btn '+ listItemRemoveButtonClass + '" type="button"><i class="icon-remove"></i></button>' +
+                '</div>' +
+            '</li>';
+        return $( item );
     }
 
     /**
@@ -324,17 +369,11 @@ function DiffuserForm( parentId, diffusersUri, settings ) {
             containingClassName : $( "#" + config.containingClassId ).val(),
             methodName : $( "#" + config.methodNameId ).val(),
             returnTypeClassName : getReturnType(),
-            argumentTypes : getValues( config.methodArgListId ),
-            classPaths : getValues( config.classPathListId ),
+            argumentTypes : getValues( $( "#" + config.methodArgListId ) ),
+            classPaths : getValues( $( "#" + config.classPathListId ) ),
             serializerType : getSerializerName(),
-            clientEndpoints : getValues( config.endPointListId )
+            clientEndpoints : getValues( $( "#" + config.endPointListId ) )
         };
         return JSON.stringify( formValues );
-    }
-
-    // the exposed diffuser form methods
-    return {
-        asJson : asJson(),
-        resetForm : resetDiffuserForm()
     }
 }
--- manager/WebContent/scripts/serializer-list.js
+++ manager/WebContent/scripts/serializer-list.js
@@ -1,21 +1,25 @@
 /**
- * User: rob
- * Date: 1/2/13
- * Time: 7:31 PM
+ * Loads the list of serializer names into the selection specified by the serializerListId.
+ * If there is an error, the default name is used.
+ * @param serializersUri The URI from which to load the list of serializer names
+ * @param serializerListElement The jQuery element to which to add the serializer name options
+ * @param defaultSerializer The name of the default serializer.
  */
-function getSerializerList( serializersUri, serializerListId, defaultSerializer ) {
-    var defaultSerializer = "persistence_xml" || defaultSerializer;
+function getSerializerList( serializersUri, serializerListElement, defaultSerializer ) {
+    if( !defaultSerializer ) {
+        defaultSerializer = "persistence_xml";
+    }
     $.get( serializersUri, function( xml ) {
         $(xml).find( "entry" ).each( function() {
             var option = $( this ).find( "title" ).text();
             if( option === defaultSerializer ) {
-                $( serializerListId ).append( '<option selected>' + option + '</option>' );
+                serializerListElement.append( '<option selected>' + option + '</option>' );
             } else {
-                $( serializerListId ).append( '<option>' + option + '</option>' );
+                serializerListElement.append( '<option>' + option + '</option>' );
             }
         });
     }, "xml" ).error( function() {
-            $( serializerListId ).append( '<option>' + defaultSerializer + '</option>' );
+            serializerListElement.append( '<option>' + defaultSerializer + '</option>' );
             alert( 'failed to load serializer names' )
-        });
+    });
 }
